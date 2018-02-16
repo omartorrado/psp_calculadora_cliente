5ea884aad53eaeb6598f9eaf348c2f9c214145f1
omartorrado <frikimoro@gmail.com>
2018-02-16 14:25:02 +0100

    finalizada, listas todas las comprobaciones para evitar que de errores

--- src/psp_calculadora_cliente/UI.java
+++ src/psp_calculadora_cliente/UI.java
@@ -23,6 +23,7 @@ public class UI extends javax.swing.JFrame {
 
     /*
     estados: 0 primer numero, 1 segundo numero, 2 acabas de escribir el operador
+    3 acabamos de darle a =
      */
     int estadoOperacion = 0;
 
@@ -68,19 +69,19 @@ public class UI extends javax.swing.JFrame {
         jButtonC = new javax.swing.JButton();
         jButtonPunto = new javax.swing.JButton();
         jButtonIgual = new javax.swing.JButton();
-        jButtonMasMenos = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(255, 204, 204));
+        jPanel1.setBackground(new java.awt.Color(0, 51, 51));
         jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        filler1.setBackground(new java.awt.Color(255, 255, 255));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 2;
         jPanel1.add(filler1, gridBagConstraints);
 
-        jLabelPantallaTop.setText("jLabel1");
         jLabelPantallaTop.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jLabelPantallaTop.setMaximumSize(new java.awt.Dimension(500, 50));
         jLabelPantallaTop.setOpaque(true);
@@ -95,7 +96,7 @@ public class UI extends javax.swing.JFrame {
 
         jLabelPantallaPrincipal.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabelPantallaPrincipal.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        jLabelPantallaPrincipal.setText("label 2");
+        jLabelPantallaPrincipal.setText("0");
         jLabelPantallaPrincipal.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jLabelPantallaPrincipal.setMaximumSize(new java.awt.Dimension(500, 50));
         jLabelPantallaPrincipal.setOpaque(true);
@@ -108,7 +109,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 0);
         jPanel1.add(jLabelPantallaPrincipal, gridBagConstraints);
 
-        jButton1.setBackground(new java.awt.Color(255, 204, 204));
+        jButton1.setBackground(new java.awt.Color(204, 204, 204));
         jButton1.setText("1");
         jButton1.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton1.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -123,7 +124,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 2;
         jPanel1.add(jButton1, gridBagConstraints);
 
-        jButton2.setBackground(new java.awt.Color(255, 204, 204));
+        jButton2.setBackground(new java.awt.Color(204, 204, 204));
         jButton2.setText("2");
         jButton2.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton2.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -138,7 +139,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 2;
         jPanel1.add(jButton2, gridBagConstraints);
 
-        jButton3.setBackground(new java.awt.Color(255, 204, 204));
+        jButton3.setBackground(new java.awt.Color(204, 204, 204));
         jButton3.setText("3");
         jButton3.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton3.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -153,7 +154,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 2;
         jPanel1.add(jButton3, gridBagConstraints);
 
-        jButton7.setBackground(new java.awt.Color(255, 204, 204));
+        jButton7.setBackground(new java.awt.Color(204, 204, 204));
         jButton7.setText("7");
         jButton7.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton7.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -168,7 +169,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 4;
         jPanel1.add(jButton7, gridBagConstraints);
 
-        jButton0.setBackground(new java.awt.Color(255, 204, 204));
+        jButton0.setBackground(new java.awt.Color(204, 204, 204));
         jButton0.setText("0");
         jButton0.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton0.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -183,7 +184,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 5;
         jPanel1.add(jButton0, gridBagConstraints);
 
-        jButton4.setBackground(new java.awt.Color(255, 204, 204));
+        jButton4.setBackground(new java.awt.Color(204, 204, 204));
         jButton4.setText("4");
         jButton4.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton4.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -198,7 +199,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 3;
         jPanel1.add(jButton4, gridBagConstraints);
 
-        jButton5.setBackground(new java.awt.Color(255, 204, 204));
+        jButton5.setBackground(new java.awt.Color(204, 204, 204));
         jButton5.setText("5");
         jButton5.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton5.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -213,7 +214,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 3;
         jPanel1.add(jButton5, gridBagConstraints);
 
-        jButton6.setBackground(new java.awt.Color(255, 204, 204));
+        jButton6.setBackground(new java.awt.Color(204, 204, 204));
         jButton6.setText("6");
         jButton6.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton6.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -228,7 +229,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 3;
         jPanel1.add(jButton6, gridBagConstraints);
 
-        jButtonMultiplica.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonMultiplica.setBackground(new java.awt.Color(204, 204, 204));
         jButtonMultiplica.setText("X");
         jButtonMultiplica.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonMultiplica.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -243,7 +244,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 4;
         jPanel1.add(jButtonMultiplica, gridBagConstraints);
 
-        jButtonResta.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonResta.setBackground(new java.awt.Color(204, 204, 204));
         jButtonResta.setText("-");
         jButtonResta.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonResta.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -258,7 +259,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 3;
         jPanel1.add(jButtonResta, gridBagConstraints);
 
-        jButton8.setBackground(new java.awt.Color(255, 204, 204));
+        jButton8.setBackground(new java.awt.Color(204, 204, 204));
         jButton8.setText("8");
         jButton8.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton8.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -273,7 +274,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 4;
         jPanel1.add(jButton8, gridBagConstraints);
 
-        jButton9.setBackground(new java.awt.Color(255, 204, 204));
+        jButton9.setBackground(new java.awt.Color(204, 204, 204));
         jButton9.setText("9");
         jButton9.setMaximumSize(new java.awt.Dimension(50, 50));
         jButton9.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -288,7 +289,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 4;
         jPanel1.add(jButton9, gridBagConstraints);
 
-        jButtonSuma.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonSuma.setBackground(new java.awt.Color(204, 204, 204));
         jButtonSuma.setText("+");
         jButtonSuma.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonSuma.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -303,7 +304,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 2;
         jPanel1.add(jButtonSuma, gridBagConstraints);
 
-        jButtonDivide.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonDivide.setBackground(new java.awt.Color(204, 204, 204));
         jButtonDivide.setText("/");
         jButtonDivide.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonDivide.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -318,7 +319,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 5;
         jPanel1.add(jButtonDivide, gridBagConstraints);
 
-        jButtonCE.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonCE.setBackground(new java.awt.Color(204, 204, 204));
         jButtonCE.setText("CE");
         jButtonCE.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonCE.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -333,7 +334,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 2;
         jPanel1.add(jButtonCE, gridBagConstraints);
 
-        jButtonC.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonC.setBackground(new java.awt.Color(204, 204, 204));
         jButtonC.setText("C");
         jButtonC.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonC.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -348,7 +349,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 3;
         jPanel1.add(jButtonC, gridBagConstraints);
 
-        jButtonPunto.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonPunto.setBackground(new java.awt.Color(204, 204, 204));
         jButtonPunto.setText(".");
         jButtonPunto.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonPunto.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -363,7 +364,7 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 5;
         jPanel1.add(jButtonPunto, gridBagConstraints);
 
-        jButtonIgual.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonIgual.setBackground(new java.awt.Color(204, 204, 204));
         jButtonIgual.setText("=");
         jButtonIgual.setMaximumSize(new java.awt.Dimension(50, 50));
         jButtonIgual.setMinimumSize(new java.awt.Dimension(50, 50));
@@ -378,30 +379,15 @@ public class UI extends javax.swing.JFrame {
         gridBagConstraints.gridy = 5;
         jPanel1.add(jButtonIgual, gridBagConstraints);
 
-        jButtonMasMenos.setBackground(new java.awt.Color(255, 204, 204));
-        jButtonMasMenos.setText("+/-");
-        jButtonMasMenos.setMaximumSize(new java.awt.Dimension(50, 50));
-        jButtonMasMenos.setMinimumSize(new java.awt.Dimension(50, 50));
-        jButtonMasMenos.setPreferredSize(new java.awt.Dimension(50, 50));
-        jButtonMasMenos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonMasMenosActionPerformed(evt);
-            }
-        });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 5;
-        gridBagConstraints.gridy = 4;
-        jPanel1.add(jButtonMasMenos, gridBagConstraints);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 337, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 461, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 428, Short.MAX_VALUE)
         );
 
         pack();
@@ -467,10 +453,6 @@ public class UI extends javax.swing.JFrame {
         borrarTodo();
     }//GEN-LAST:event_jButtonCEActionPerformed
 
-    private void jButtonCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCActionPerformed
-        borrarNumeroActual();
-    }//GEN-LAST:event_jButtonCActionPerformed
-
     private void jButtonPuntoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonPuntoActionPerformed
         escribirPuntoDecimal();
     }//GEN-LAST:event_jButtonPuntoActionPerformed
@@ -479,9 +461,9 @@ public class UI extends javax.swing.JFrame {
         resultado();
     }//GEN-LAST:event_jButtonIgualActionPerformed
 
-    private void jButtonMasMenosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonMasMenosActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButtonMasMenosActionPerformed
+    private void jButtonCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCActionPerformed
+        borrarNumeroActual();
+    }//GEN-LAST:event_jButtonCActionPerformed
 
     /**
      * @param args the command line arguments
@@ -517,16 +499,27 @@ public class UI extends javax.swing.JFrame {
             }
         });
     }
+    
+    /*
+    AQUI ESTAN LOS METODOS QUE CONTROLAN LA CALCULADORA
+    */
 
     public void escribirNumero(int numero) {
         //si lo anterior fue un operador, pasamos a escribir el segundo numero
         if (estadoOperacion == 2) {
-            estadoOperacion = 1;
-            jLabelPantallaTop.setText(numeros1);
+            estadoOperacion = 1;            
+        }else if(estadoOperacion==3){
+            estadoOperacion=0;
+            numeros1="";
         }
         //En la string temporal vamos a ir guardando los numeros que se meten seguidos, hasta que le damos a operar o al igual
         stringTemp = stringTemp + numero;
         jLabelPantallaPrincipal.setText(stringTemp);
+        if(estadoOperacion==0){
+            jLabelPantallaTop.setText(stringTemp);
+        }else{
+            jLabelPantallaTop.setText(numeros1+operador+stringTemp);
+        }
     }
 
     public void escribirPuntoDecimal() {
@@ -542,6 +535,17 @@ public class UI extends javax.swing.JFrame {
         } else {
             stringTemp = stringTemp + ".";
         }
+        //tras las comprobaciones ponemos el texto en pantalla
+        if(estadoOperacion==0){
+            jLabelPantallaTop.setText(stringTemp);
+            jLabelPantallaPrincipal.setText(stringTemp);
+        }else if(estadoOperacion==3){
+            
+        }
+        else{
+            jLabelPantallaTop.setText(numeros1+operador+stringTemp);
+            jLabelPantallaPrincipal.setText(stringTemp);
+        }
     }
 
     public void operacion(String op) {
@@ -551,42 +555,45 @@ public class UI extends javax.swing.JFrame {
          */
         //Antes e nada comprobamos que si hemos puesto el punto decimal lo siga algun numero, y sino lo quitamos
         if (stringTemp.endsWith(".")) {
-            stringTemp = stringTemp.substring(0, stringTemp.length());
-            System.out.println("Si acaba en punto imprimimos: " + stringTemp);
+            stringTemp = stringTemp.substring(0, stringTemp.length());            
         }
 
         if (estadoOperacion == 0 && !stringTemp.isEmpty()) {
-            numeros1 = stringTemp;
-            System.out.println("Primer numero: "+numeros1);
+            numeros1 = stringTemp;            
             stringTemp = "";
             estadoOperacion = 2;
             operador = op;
+            jLabelPantallaPrincipal.setText(stringTemp);
+            jLabelPantallaTop.setText(numeros1+operador);
         } else if (estadoOperacion == 1 && !stringTemp.isEmpty()) {
             resultado();
             operador = op;
-        }
-        if (estadoOperacion == 2) {
+            estadoOperacion=2;
+        }else if (estadoOperacion == 2 || estadoOperacion==3) {
             operador = op;            
             jLabelPantallaTop.setText(numeros1+operador);
+            estadoOperacion=2;
         }
 
     }
 
     public void resultado() {
-        if (estadoOperacion ==1) {
-            numeros2 = stringTemp;
-            System.out.println("Segundo numero: "+numeros2);
+        if (estadoOperacion ==1&&!operador.isEmpty()) {
+            numeros2 = stringTemp;            
             mensajeParaServidor = numeros1 + "," + numeros2 + "," + operador;
-            System.out.println("Mensaje:"+mensajeParaServidor);
-            String mensaje = cliente.ejecutar(mensajeParaServidor);
-            jLabelPantallaTop.setText(numeros1 + operador + numeros2 + "=");
-            jLabelPantallaPrincipal.setText(mensaje.trim());
+            System.out.println("Mensaje: "+numeros1+operador+numeros2);
+            String mensaje = cliente.ejecutar(mensajeParaServidor);            
             respuesta = mensaje.trim();
+            if(respuesta.endsWith(".0")){
+                respuesta=respuesta.substring(0, respuesta.length()-2);
+            }
+            jLabelPantallaTop.setText(numeros1 + operador + numeros2 + "="+respuesta);
             numeros1 = respuesta;
             stringTemp = "";
             numeros2 = "";
             operador = "";
-            estadoOperacion = 2;
+            estadoOperacion = 3;            
+            jLabelPantallaPrincipal.setText(respuesta);
         }
     }
 
@@ -611,6 +618,9 @@ public class UI extends javax.swing.JFrame {
         //Si estabamos escribiendo el segundo numero, volvemos al estado de operador tras borrar el numero
         if (estadoOperacion == 1) {
             estadoOperacion = 2;
+            jLabelPantallaTop.setText(numeros1+operador);
+        }else if(estadoOperacion==0){
+            jLabelPantallaTop.setText("");
         }
     }
 
@@ -630,7 +640,6 @@ public class UI extends javax.swing.JFrame {
     private javax.swing.JButton jButtonCE;
     private javax.swing.JButton jButtonDivide;
     private javax.swing.JButton jButtonIgual;
-    private javax.swing.JButton jButtonMasMenos;
     private javax.swing.JButton jButtonMultiplica;
     private javax.swing.JButton jButtonPunto;
     private javax.swing.JButton jButtonResta;
